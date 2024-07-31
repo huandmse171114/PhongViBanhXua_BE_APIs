393faba484b56bf0699de7aa7eafbae069c755a2
huandmse171114 <huandmse171114@fpt.edu.vn>
2024-07-31 15:50:06 +0700

    first commit

---
+++ src/main/java/com/phongvi/PhongViBanhXuaApplication.java
@@ -0,0 +1,13 @@
+package com.phongvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongViBanhXuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhongViBanhXuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phongvi/PhongViBanhXuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhongViBanhXuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
